0955e6f6782dd080207e2f28c184aec68a44ed9d
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-11-04 21:35:48 +0100

    Mateusz Hamera

---
+++ index.js
@@ -0,0 +1,23 @@
+//const { response, request } = require('express');
+const express = require('express');
+const ejs = require('ejs')
+const config = require('./public/js/config').config;
+const app = express();
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname+'public/css'))
+app.use('/cjs', express.static(__dirname+'public/js'))
+app.use('/img', express.static(__dirname+'public/img'))
+
+app.set('view engine','html');
+app.engine('html',ejs.renderFile);
+
+
+app.get('/', (request, response) => {
+	response.render(__dirname+'/views/index.html',
+	{subject: 'Technologie webowe w aplikacjach Internetu'});
+});
+
+app.listen(config.port, function(){
+	console.info('Serwer is running at port 3000')
+});
