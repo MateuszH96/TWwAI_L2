3309eee1fb9551c42df3a6df494f8ece89846377
Mateusz Hamera <mateuszhamera3@gmail.com>
2022-11-18 13:07:43 +0100

    laboratorium5

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const ejs = require('ejs')
-const config = require('./public/js/config').config;
-const app = express();
-
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname+'public/css'))
-app.use('/cjs', express.static(__dirname+'public/js'))
-app.use('/img', express.static(__dirname+'public/img'))
-
-app.set('view engine','html');
-app.engine('html',ejs.renderFile);
+require( 'babel-core/register' );
+require( './app/app');
 
 
-app.get('/', (request, response) => {
-	response.render(__dirname+'/views/index.html',
-	{subject: 'Nazwa zmieniającego się nagłowka'});
-});
 
-app.listen(config.port, function(){
-	console.info('Serwer is running at port 3000')
-});
+// const express = require('express');
+// const ejs = require('ejs')
+// const config = require('./public/js/config').config;
+// const app = express();
+
+// app.use(express.static('public'));
+// app.use('/css', express.static(__dirname+'public/css'))
+// app.use('/cjs', express.static(__dirname+'public/js'))
+// app.use('/img', express.static(__dirname+'public/img'))
+
+// app.set('view engine','html');
+// app.engine('html',ejs.renderFile);
+
+
+// app.get('/', (request, response) => {
+// 	response.render(__dirname+'/views/index.html',
+// 	{subject: 'Nazwa zmieniającego się nagłowka'});
+// });
+
+// app.listen(config.port, function(){
+// 	console.info('Serwer is running at port 3000')
+// });
